a07ba97fb87770ef2896d132b42bcef2e394f558
Salvydas Lukosius <sall@w-ss.io>
2022-05-16 18:45:20 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ async function serveAsset(event) {
     response = await fetch(`${BUCKET_URL}${url.pathname}`)
     const headers = {
       'Cache-Control': 'must-revalidate, public, max-age=15552000',
+      'Access-Control-Allow-Origin': '*'.
+      'X-Robots-Tag': 'nosnippet',
     }
     response = new Response(response.body, { ...response, headers })
     event.waitUntil(cache.put(event.request, response.clone()))
