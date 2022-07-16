8f48f47cf3b61c232bfa6924a081cd52b4f6e749
Salvydas Lukosius <sall@w-ss.io>
2022-07-16 08:54:43 +0100

    Update index.ts
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event))
+  event.respondWith(handleRequest(event));
 })
 
-const BUCKET_NAME = 'digital-space'
-const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`
+const BUCKET_NAME = 'digital-space';
+const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`;
 
 async function serveAsset(event) {
-  const url = new URL(event.request.url)
-  const cache = caches.default
-  let response = await cache.match(event.request)
+  const url = new URL(event.request.url);
+  const cache = caches.default;
+  let response = await cache.match(event.request);
+
   if (!response) {
-    response = await fetch(`${BUCKET_URL}${url.pathname}`)
-    const headers = {
-      'Cache-Control': 'must-revalidate, public, max-age=15552000',
-      'Content-Type': 'application/json;charset=UTF-8',
-    }
-    response = new Response(response.body, { ...response, headers })
-    event.waitUntil(cache.put(event.request, response.clone()))
+    response = await fetch(`${BUCKET_URL}${url.pathname}`);
+    const headers = { 'cache-control': 'public, max-age=14400' };
+    response = new Response(response.body, { ...response, headers });
+    event.waitUntil(cache.put(event.request, response.clone()));
   }
-  return response
+  return response;
 }
+
 async function handleRequest(event) {
   if (event.request.method === 'GET') {
-    let response = await serveAsset(event)
+    let response = await serveAsset(event);
     if (response.status > 399) {
-      response = new Response(response.statusText, { status: response.status })
+      response = new Response(response.statusText, { status: response.status });
     }
-    return response
+    return response;
   } else {
-    return new Response('Method not allowed', { status: 405 })
+    return new Response('Method not allowed', { status: 405 });
   }
 }
