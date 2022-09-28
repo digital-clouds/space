16018c80caa36089b37bbf017fe9bbd2c7c774d2
Salvydas Lukosius <sall@w-ss.io>
2022-09-28 23:00:38 +0100

    Update index.ts
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event));
-})
-
-const BUCKET_NAME = 'digital-space';
-const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`;
+addEventListener("fetch", async (event) => {
+  try {
+    return event.respondWith(handleRequest(event));
+  } catch (e) {
+    return event.respondWith(new Response(`Error thrown ${e.message}`));
+  }
+});
 
 async function serveAsset(event) {
-  const url = new URL(event.request.url);
+  const request = event.request;
+  const url = new URL(request.url);
   const cache = caches.default;
-  let response = await cache.match(event.request);
-
+  let response = await cache.match(request);
   if (!response) {
-    response = await fetch(`${BUCKET_URL}${url.pathname}`);
-    const headers = { 'cache-control': 'public, max-age=14400' };
+  const BUCKET_NAME = 'digital-space';
+  const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`;
+    response = await fetch(`${HOST_URL}${url.pathname}`);
+    const headers = {
+      "cache-control": "public, max-age=14400, s-maxage=84000",
+      "x-goog-project-id": "digital-clouds",
+    };
     response = new Response(response.body, { ...response, headers });
-    event.waitUntil(cache.put(event.request, response.clone()));
+    event.waitUntil(cache.put(request, response.clone()));
   }
   return response;
 }
 
 async function handleRequest(event) {
-  if (event.request.method === 'GET') {
+  const request = event.request;
+  if (request.method === "GET") {
     let response = await serveAsset(event);
     if (response.status > 399) {
       response = new Response(response.statusText, { status: response.status });
     }
     return response;
   } else {
-    return new Response('Method not allowed', { status: 405 });
+    return new Response("Method not allowed", { status: 405 });
   }
 }
