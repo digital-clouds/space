87ffd5ed26e82b8a741b2705b26d5976770d8b2a
Salvydas Lukosius <sall@w-ss.io>
2022-12-23 17:52:58 +0000

    deleted:    package.json 	deleted:    pnpm-lock.yaml

---
+++ workers/digital-space/src/index.ts
@@ -0,0 +1,39 @@
+// Description: Digital Space Worker
+
+export default {
+	async fetch(request, env, context) {
+		const BUCKET_NAME = 'digital-space';
+		const BUCKET_HOST = `https://storage.googleapis.com/${BUCKET_NAME}`;
+
+		if (request.method === 'GET') {
+			const url = new URL(request.url);
+			const cacheKey = new Request(url.toString(), {
+				headers: request.headers,
+				method: 'GET',
+				cf: request.cf,
+			});
+			const cache = caches.default;
+			let response = await cache.match(cacheKey);
+
+			if (!response) {
+				response = await fetch(`${BUCKET_HOST}${url.pathname}`);
+				const headers = {
+					'Cache-Control': 'public, max-age=14400',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Headers': '*',
+					'Access-Control-Allow-Methods': 'GET, OPTIONS',
+					'Access-Control-Max-Age': '86400',
+					'x-goog-project-id': 'digital-clouds',
+				};
+				response = new Response(response.body, { ...response, headers });
+				context.waitUntil(cache.put(cacheKey, response.clone()));
+			}
+			if (response.status > 399) {
+				response = new Response(response.statusText, { status: response.status });
+			}
+			return response;
+		} else {
+			return new Response('Method not allowed', { status: 405 });
+		}
+	},
+};
