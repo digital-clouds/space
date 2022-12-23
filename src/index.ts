d0fe98bdac3cc7f56839d796b2e9be66d94b73e9
Salvydas Lukosius <sall@w-ss.io>
2022-12-23 14:21:19 +0000

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,40 @@
-export default {
-  async fetch(request: Request, env: any, context: any): Promise<Response> {
-    const name = 'digital-space'
-    const bucket = `https://storage.googleapis.com/${name}`
-
-    if (request.method === 'GET') {
-      const url = new URL(request.url)
-      const cache = caches.default
-      const cacheKey = new Request(url.toString(), request)
-      let response = await cache.match(cacheKey)
-
-      if (!response) {
-        response = await fetch(request)
-        response = await fetch(`${bucket}${url.pathname}`)
-        const headers = {
-          'cache-Control': 'public, max-age=14400, s-maxage=84000',
-          'x-goog-project-id': 'digital-clouds',
-          'access-control-allow-origin': '*',
-        }
-        response = new Response(response.body, { ...response, headers })
-        context.waitUntil(cache.put(cacheKey, response.clone()))
-      }
-
-      if (response.status > 399) {
-        response = new Response(response.statusText, { status: response.status })
-      }
-      return response
-    } else {
-      return new Response('Method not allowed', { status: 405 })
+addEventListener('fetch', async (event) => {
+  try {
+    return event.respondWith(handleRequest(event))
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return event.respondWith(new Response(`Error thrown ${err.message}`))
     }
-  },
+  }
+})
+
+async function serveAsset(event: FetchEvent): Promise<Response> {
+  const request = event.request
+  const url = new URL(request.url)
+  const cache = caches.default
+  let response = await cache.match(request)
+  if (!response) {
+    const BUCKET_NAME = 'digital-space'
+    const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`
+    response = await fetch(`${BUCKET_URL}${url.pathname}`)
+    const headers = { 'cache-control': 'public, max-age=14400, s-maxage=84000' }
+    response = new Response(response.body, { ...response, headers })
+    event.waitUntil(cache.put(request, response.clone()))
+  }
+  return response
 }
+
+async function handleRequest(event: FetchEvent): Promise<Response> {
+  const request = event.request
+  if (request.method === 'GET') {
+    let response = await serveAsset(event)
+    if (response.status > 399) {
+      response = new Response(response.statusText, { status: response.status })
+    }
+    return response
+  } else {
+    return new Response('Method not allowed', { status: 405 })
+  }
+}
+
+export {}
